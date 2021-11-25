bab51ac69fb7f9289da11b2dab56a01dce912cca
lasmavisnevska <94110629+lasmavisnevska@users.noreply.github.com>
2021-11-25 10:52:52 +0000

    funk

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ print('ta - ja figura ir taisnstūris')
 print('k - ja figura ir kvadrāts')
 print('R -  ja figūra ir riņķis')
 print('_________________________________________________________')
+
+def taisnsturis():
+  a = int(input("Ievadi ta garumu"))
+  b = int(input("Ievadi ta platumu"))
+  laukums4 = a*b
+  print(laukums4)
+
+def kvadrats():
+  a = int(input("Ievadi kvadrāta malas garumu"))
+  laukums5 = a*a
+  print(laukums5)
+
+def rinkis():
+  a = int(input("Ievadi riņķa pī")
