eacdb9e14efb7d0521ca602cc8ae816453a03ef3
Ričards Matīss Matīss <94109259+RicardsArins@users.noreply.github.com>
2021-11-25 10:58:04 +0000

    add figuru laukumus

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ print('ta - ja figura ir taisnstūris')
 print('k - ja figura ir kvadrāts')
 print('R -  ja figūra ir riņķis')
 print('_________________________________________________________')
+def trijsturis():
+  a = int(input('ievadi malas garumu'))
+  h = int(input("ievadi trijstūra augstumu"))
+  laukums1 = a*h 
+  print(laukums1)
+def trapece():
+  pamats1 = int(input('ievadi pirmā pamata garumu'))
+  pamats2 = int(input('ievadi otrā pamata garumu'))
+  h = int(input("ievadi trapeces augstumu")) 
+  laukums2 = ((pamats1 + pamats2)* h) / 2
+  print(laukums2)
+def paralelograms():
+  malas_garums = int(input("ievadi paralelograma malas garumu"))
+  augstums = int(input("ievadi augstumu kas novilkts perpendikulāri malas garumam"))
+  laukums3 = malas_garums * augstums
+  print(laukums3)
+
+figura = int(input('ievadi izvēlēto figūras saīsinājumu, kurus vēlies aprēkināt'))
+
